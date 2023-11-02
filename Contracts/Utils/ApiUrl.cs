499a10ac81d4e220e9a7495991ac9b0f11c08b2a
Ludmila Arias <ludmila062001@gmail.com>
2023-11-02 14:59:02 -0300

    Arreglos

---
+++ Contracts/Utils/ApiUrl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Utils
+{
+    public static class ApiUrl
+    {
+        public static string AzureUrl { get; } = "https://moto-top.azurewebsites.net/api/";
+        public static string LocalUrl { get; } = "https://localhost:7215/api/";
+    }
+}
